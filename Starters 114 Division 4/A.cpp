223ffcc5d7e95719e42074af475a9873613885a8
lengmuzashi <2690497440@qq.com>
2023-12-27 23:34:05 +0800

    Starters 114 Division 4

---
+++ Starters 114 Division 4/A.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 0;
+	cin >> x;
+	if (x == 25)
+	{
+		cout << "CHRISTMAS";
+	}
+	else
+	{
+		cout << "ORDINARY";
+	}
+}
